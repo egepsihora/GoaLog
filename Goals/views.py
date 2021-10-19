0a725ac98a9c57e694535b90faae33e7fde06751
egepsihora <zeland18@ukr.net>
2021-10-19 13:09:10 +0300

    Calendar was added

--- Goals/views.py
+++ Goals/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Goal
 
 
-def goal_list(request):
-    goals = Goal
-    return render(request, 'list.html', {'goals': goals})
+# def index(request):
+#     return HttpResponse("<h3>hello</h3>")
+
+
+def index(request):
+    goals = []
+    for goal in Goal.objects.all():
+        goals.append(goal.Name)
+    return render(request, 'Goals/goals.html', {'goals': goals})
+
